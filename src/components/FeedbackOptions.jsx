d430f2cd2cc78470e98a85ca4973741a0b591a59
MarynaVCH <83900520+MarynaVCH@users.noreply.github.com>
2022-01-27 01:12:14 +0200

    final task

---
+++ src/components/FeedbackOptions.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function FeedbackOptions({ options, onLeaveFeedback }) {
+  const data = Object.keys(options);
+  return (
+    <div>
+      {data.map(feedback => (
+        <button
+          key={feedback}
+          type="button"
+          data-action={feedback}
+          onClick={onLeaveFeedback}
+        >
+          {feedback}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.object.isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
